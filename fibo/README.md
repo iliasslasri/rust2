9a3b90e33a1abc5a4c60aedcf87717c2848ca69f
Iliass Lasri <iliass.lasri@telecom-paris.fr>
2024-02-14 17:32:52 +0100

    Recursive Implementation of the fibo Function

---
+++ fibo/README.md
@@ -0,0 +1,15 @@
+
+In the debug mode we have :
+
+```
+fibo(42) = 267914296
+Time elapsed: 5.052903101s
+```
+
+Where in the realease mode we have :
+```
+fibo(42) = 267914296
+Time elapsed: 1.517708928s
+```
+
+###### The release mode is 3.33 times faster than the debug mode.
---
+++ fibo/src/main.rs
@@ -0,0 +1,18 @@
+use std::time::Instant;
+
+fn main() {
+    let start = Instant::now();
+    for i in 0..=42 {
+        println!("fibo({}) = {}",i, fibo(i));
+    }
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
+}
+
+fn fibo(n: u32) -> u32 {
+    if n == 0{0}
+    else if n == 1{1}
+    else {
+        fibo(n-1) + fibo(n-2)
+    }
+}
