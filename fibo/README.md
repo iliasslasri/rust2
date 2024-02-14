542fb1f5e2435fbbff2927dcf8e9f264d9d91b5f
Iliass Lasri <iliass.lasri@telecom-paris.fr>
2024-02-14 18:06:56 +0100

    Iterative Implementation

--- fibo/README.md
+++ fibo/README.md
@@ -13,3 +13,13 @@ Time elapsed: 1.517708928s
 ```
 
 ###### The release mode is 3.33 times faster than the debug mode.
+
+
+When we change the max value to 50, the program stops with the following error:
+```
+fibo(47) = 2971215073
+thread 'main' panicked at src/main.rs:20:17:
+attempt to add with overflow
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+```
+This is because the next number in the sequence is too large to be stored in a 32-bit unsigned integer, so it overflows.
--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,20 +1,22 @@
-use std::time::Instant;
-
 fn main() {
-    let start = Instant::now();
-    for i in 0..=42 {
+    for i in 0..=50 {
         println!("fibo({}) = {}", i, fibo(i));
     }
-    let duration = start.elapsed();
-    println!("Time elapsed: {:?}", duration);
 }
-
 fn fibo(n: u32) -> u32 {
     if n == 0 {
         0
     } else if n == 1 {
         1
     } else {
-        fibo(n - 1) + fibo(n - 2)
+        let mut a = 0;
+        let mut b = 1;
+        let mut c = 0;
+        for _ in 0..n {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        c
     }
 }
