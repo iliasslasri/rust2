e06aed9f27434ebb25e23832f0bcf1b471994213
Iliass Lasri <iliass.lasri@telecom-paris.fr>
2024-02-27 10:51:48 +0100

    OOR (owned or ref) type

--- problems/src/main.rs
+++ problems/src/main.rs
@@ -1,26 +1,79 @@
-fn ret_string() -> String {
-    String::from("  A String object  ")
-}
-
-fn main() {
-    let binding = ret_string();
-    let s = binding.trim(); // returns a new string with the whitespace trimmed
-    // The problem with let s = ret_string().trim(); is that the ret_sting() returns a String object, 
-    // and the trim() method allocates a new string object, so the original string object is not modified, the value 
-    // is fropped while borrowed.
-    // temporary value is freed at the end of this statement
-    //creates a temporary value which is freed while still in use so there is no variable that ownes the string that is still in use
-    // let s = ret_string().trim();
-    assert_eq!(s, "A String object");
-    println!(
-        "The original string is not modified, the value is borrowed: {} ",
-        binding
-    )
-}
+use std::ops::Deref;
+use std::ops::DerefMut;
 
 /* Appropriate lifetimes */
 // Since at call time, we don't want any constraint between the lifetimes of those two strings,
 // we just infer the smallest lifetime to our return reference.
 fn choose_str<'a>(s1: &'a str, s2: &'a str, select_s1: bool) -> &'a str {
-    if select_s1 { s1 } else { s2 }
+    if select_s1 {
+        s1
+    } else {
+        s2
+    }
+}
+
+// OOR (Owned or Reference) type
+enum OOR<'a> {
+    Borrowed(&'a str),
+    Owned(String),
+}
+
+// Deref trait for OOR
+impl<'a> Deref for OOR<'a> {
+    type Target = str;
+    fn deref(&self) -> &str {
+        match *self {
+            OOR::Borrowed(s) => s,
+            OOR::Owned(ref s) => s,
+        }
+    }
+}
+
+// DrefMut trait for OOR
+impl<'a> DerefMut for OOR<'a> {
+    fn deref_mut(&mut self) -> &mut str {
+        match *self {
+            OOR::Borrowed(str) => {
+                let mut s = String::from("");
+                // swaps the content of s with the content of String::from(str). 
+                // This effectively transfers ownership of the string from str to s.
+                std::mem::swap(&mut s, &mut String::from(str));
+                // We can also use
+                // let mut s;
+                // s = std::mem::take(&mut String::from(str));
+                // in both cases we take the ownership of the string from str to s
+                *self = OOR::Owned(s);
+                self
+            }
+            OOR::Owned(ref mut s) => s,
+        }
+    }
+}
+
+fn main() {
+    // Check Deref for both variants of OOR
+    let s1 = OOR::Owned(String::from("  Hello, world.  "));
+    assert_eq!(s1.trim(), "Hello, world.");
+    let mut s2 = OOR::Borrowed("  Hello, world!  ");
+    assert_eq!(s2.trim(), "Hello, world!");
+
+    // Check choose
+    let s = choose_str(&s1, &s2, true);
+    assert_eq!(s.trim(), "Hello, world.");
+    let s = choose_str(&s1, &s2, false);
+    assert_eq!(s.trim(), "Hello, world!");
+
+    // Check DerefMut, a borrowed string should become owned
+    assert!(matches!(s1, OOR::Owned(_)));
+    assert!(matches!(s2, OOR::Borrowed(_)));
+    unsafe {
+        for c in s2.as_bytes_mut() {
+            if *c == b'!' {
+                *c = b'?';
+            }
+        }
+    }
+    assert!(matches!(s2, OOR::Owned(_)));
+    assert_eq!(s2.trim(), "Hello, world?");
+    println!("All tests passed!")
 }
